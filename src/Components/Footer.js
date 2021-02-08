786da61a769388d619f154ad774ae08e2b236e87
RuariDouglas <70585404+RuariDouglas@users.noreply.github.com>
2021-02-08 18:28:50 +0100

    Final styling fix's

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
+// STYLES
+import logo from "../Images/logo.png";
 
 const Footer = () => {
-  return <div>This is the footer</div>;
+  return (
+    <footer className="footer">
+      <div className="wrapper">
+        <div className="nav__logo-wrapper">
+          <Link to="/">
+            <img src={logo} alt="The Guardian Logo" />
+          </Link>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
